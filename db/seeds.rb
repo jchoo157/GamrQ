9f202718b124b70977aa4875c4e838bca7e21a97
APilarca <angelpill15@gmail.com>
2017-01-21 16:24:24 -0800

    create seed file for users and friendships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+genders = ['Female', 'Male', 'Agender', 'Gender fluid', 'Gender Queer', 'Intersex', 'Non Binary', 'N/A']
+
+20.times {User.create!(name: Faker::Name.first_name, age: rand(18..50), gender: genders.sample, summary: Faker::Hipster.sentence)}
+
+user_array = User.all
+
+user_array.each do |user|
+	Friendship.create!(user_id: user.id, friend_id: rand(0..user_array.length))
+end
